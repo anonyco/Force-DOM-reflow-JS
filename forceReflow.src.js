f1598cdb97603671ad2fe298e0a716499dcae177
Jack Giffin <25795277+anonyco@users.noreply.github.com>
2018-11-28 17:11:34 -0500

    Update forceReflow.src.js

--- forceReflow.src.js
+++ forceReflow.src.js
@@ -1,7 +1,11 @@
 // forces a complete reflow of a single element
-window.forceReflowJS = Function.prototype.call.bind( // wrap it all up into an auto
-  Object.getOwnPropertyDescriptor( // obtain the internal funciton that performs the reflow
-    HTMLElement.prototype,
-    "offsetHeight"
-  ).get
-);
+try {
+	window.forceReflowJS = Function.prototype.call.bind( // wrap it all up into an auto
+		Object.getOwnPropertyDescriptor( // obtain the internal funciton that performs the reflow
+			HTMLElement.prototype,
+			"offsetHeight"
+		).get
+	);
+} catch(e) {
+	window.forceReflowJS = function(a{void a.offsetHeight};
+}
